559dc4493ee6c8f0656a5fbe733b4e4f77b154e9
gangadhara-at-820046810644 <gs@nixbees.com>
2019-08-23 08:38:52 +0530

    hill cipher working

--- cns/hill_cipher/3_hill_cipher.cpp
+++ cns/hill_cipher/3_hill_cipher.cpp
@@ -4,40 +4,24 @@ Author: Gangadhara Shetty P J
 B
 Algorithm
 ---------
-    The 'key' for a playfair cipher is generally a word, for the sake of example we will choose 'monarchy'. 
-	This is then used to generate a 'key square'. Any sequence of 25 letters can be used as a key, 
-	so long as all letters are in it and there are no repeats. Note that there is no 'j', it is combined with 'i'. 
-	We now apply the encryption rules to encrypt the plaintext.
+   Hill cipher is a polygraphic substitution cipher based on linear algebra.Each letter is represented by a number modulo 26. 
+   Often the simple scheme A = 0, B = 1, …, Z = 25 is used, but this is not an essential feature of the cipher. 
+   To encrypt a message, each block of n letters (considered as an n-component vector) is multiplied by an invertible n × n matrix, against modulus 26. 
+   To decrypt the message, each block is multiplied by the inverse of the matrix used for encryption.
 
-    1. Remove any punctuation or characters that are not present in the key square (this may mean spelling out numbers, punctuation etc.).
-    2. Identify any double letters in the plaintext and replace the second occurence with an 'x' e.g. 'hammer' -> 'hamxer'.
-    3. If the plaintext has an odd number of characters, append an 'x' to the end to make it even.
-    4. Break the plaintext into pairs of letters, e.g. 'hamxer' -> 'ha mx er'
-    5. The algorithm now works on each of the letter pairs.
-    6. Locate the letters in the key square, (the examples given are using the key square above)
-
-        a. If the letters are in different rows and columns, replace the pair with the letters on the same row respectively 
-		   but at the other pair of corners of the rectangle defined by the original pair. 
-		   The order is important – the first encrypted letter of the pair is the one that lies on the same row as the first plaintext letter.
-
-        b. If the letters appear on the same row of the table, replace them with the letters to their immediate right respectively 
-		   (wrapping around to the left side of the row if a letter in the original pair was on the right side of the row).
-
-        c. If the letters appear on the same column of the table, replace them with the letters immediately below respectively 
-		   (wrapping around to the top side of the column if a letter in the original pair was on the bottom side of the column).
+	The matrix used for encryption is the cipher key, and it should be chosen randomly from the set of invertible n × n matrices (modulo 26).
 
 */
 
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-float keyMatrix[100][100], inverseMatrix[100][100];
+int keyMatrix[100][100], inverseMatrix[100][100];
 int order;
 
 string FormatMessage(string message)
 {
-    // STEP 1 in algorithm
     for(int i=0;i<message.length();i++)
     {
         if(message[i] == ' ')   // add conditions here for any other special characters
@@ -46,22 +30,44 @@ string FormatMessage(string message)
             message = message.replace(i, 1, "i");
     }
 
-    // STEP 2 in algorithm
+	
     for(int i=1;i<message.length();i++)
          if(message[i-1] == message[i])
             message = message.insert(i, "x"), i++;
-    // STEP 3 in algorithm
+		
     if(message.length()%2)
         message += "x";
     return message;
 }
 
-int GetInverseDeterminant(int a, int m)
-{
-    a = a%m;
-    for (int x=1; x<m; x++)
-        if ((a*x) % m == 1)
-            return x;
+int GetInverseDeterminant(int R , int D = 26){ //R is the remainder or determinant
+	int i =0 ;
+	int p0= 0 , p1 =1 ; 
+	int q = 1 ; 
+	int q0 , q1 ; 
+	while(R!=0){
+		q = D/R ;
+		int tempD = D ; 
+		D = R ; 
+		R = tempD%R ; 
+
+		if(i==0) { p0 = 0 ; q0 = q ; }
+		else if(i==1){ p1==1 ;q1 = q ; }
+		else{
+			int temp = p1 ;
+			p1 = (p0-p1*(q0))%26 ; 
+			if(p1<0)p1 = 26-(abs(p1)%26) ; 
+			p0 = temp ;
+			
+			q0 = q1; 
+			q1 = q ;
+		}
+//		cout<<"p0 , p1 = " << p0 <<" " <<p1<< endl; 
+
+		i++ ; 
+	}
+	p1 = (p0-p1*(q0))%26 ; 
+	return p1 ; 
 }
 
 int GetDeterminant()
@@ -73,14 +79,16 @@ int GetDeterminant()
         for(int i = 0; i < 3; i++)
             determinant = determinant + (keyMatrix[0][i] * (keyMatrix[1][(i+1)%3] * keyMatrix[2][(i+2)%3] - keyMatrix[1][(i+2)%3] * keyMatrix[2][(i+1)%3]));
 
-    if(determinant<0)
+	if(determinant<0)
         determinant = 26 - (int(-determinant)%26);
     else
-        determinant = int(-determinant)%26;
-    return GetInverseDeterminant(determinant, 26);
+        determinant = int(determinant)%26;
+	determinant = GetInverseDeterminant(determinant, 26);
+	
+    return determinant;
 }
 
-string Multiply(string msg_group, float matrix[][100])
+string Multiply(string msg_group, int matrix[][100])
 {
     string result;
     for(int i=0; i<order; i++)
@@ -112,6 +120,10 @@ void FindInverse(int determinant)
         for(int j=0; j<order; j++)
             for(int i=0; i<order; i++)
                 inverseMatrix[i][j] = ((keyMatrix[(j+1)%3][(i+1)%3] * keyMatrix[(j+2)%3][(i+2)%3]) - (keyMatrix[(j+1)%3][(i+2)%3] * keyMatrix[(j+2)%3][(i+1)%3]))*determinant;
+	 for(int j=0; j<order; j++)
+            for(int i=0; i<order; i++)
+                if(inverseMatrix[i][j] < 0 )	inverseMatrix[i][j] = 26 - int(-inverseMatrix[i][j])%26;
+				else inverseMatrix[i][j] = int(inverseMatrix[i][j]) %26;
 }
 
 string Encrypt(string message) 
@@ -169,19 +181,12 @@ int main()
 Sample Output
 -------------
 Enter the key matrix order: 3
-Enter the keys:17 17 5
-21 18 21
-2 2 19
+Enter the keys:17 17 5 
+21 18 21 
+2 2 19 
 Enter a message to be encrypted: paymoremoney
 Message: paymoremoney
 Encrypted Message: rrlmwbkaspdh
 Decrypted Message: paymoremoney
-
-
-
-
-
-
-
 
 */
