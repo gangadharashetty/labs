b6adf5e992b8abdc96f3b815fa48038b1a566fff
Akshat <mr.akshatagarwal26@gmail.com>
2019-08-25 23:52:22 +0530

    removed msg format error

--- cns/playfair_cipher/2_play_fair_cipher.cpp
+++ cns/playfair_cipher/2_play_fair_cipher.cpp
@@ -4,26 +4,26 @@ Author: Gangadhara Shetty P J
 
 Algorithm
 ---------
-    The 'key' for a playfair cipher is generally a word, for the sake of example we will choose 'monarchy'. 
-	This is then used to generate a 'key square'. Any sequence of 25 letters can be used as a key, 
-	so long as all letters are in it and there are no repeats. Note that there is no 'j', it is combined with 'i'. 
+    The 'key' for a playfair cipher is generally a word, for the sake of example we will choose 'monarchy'.
+	This is then used to generate a 'key square'. Any sequence of 25 letters can be used as a key,
+	so long as all letters are in it and there are no repeats. Note that there is no 'j', it is combined with 'i'.
 	We now apply the encryption rules to encrypt the plaintext.
 
     1. Remove any punctuation or characters that are not present in the key square (this may mean spelling out numbers, punctuation etc.).
-    2. Identify any double letters in the plaintext and replace the second occurence with an 'x' e.g. 'hammer' -> 'hamxer'.
+    2. Identify any double letters in the plaintext and replace the second occurence with an 'x' e.g. 'hammer' -> 'hamxmerx'.
     3. If the plaintext has an odd number of characters, append an 'x' to the end to make it even.
     4. Break the plaintext into pairs of letters, e.g. 'hamxer' -> 'ha mx er'
     5. The algorithm now works on each of the letter pairs.
     6. Locate the letters in the key square, (the examples given are using the key square above)
 
-        a. If the letters are in different rows and columns, replace the pair with the letters on the same row respectively 
-		   but at the other pair of corners of the rectangle defined by the original pair. 
+        a. If the letters are in different rows and columns, replace the pair with the letters on the same row respectively
+		   but at the other pair of corners of the rectangle defined by the original pair.
 		   The order is important – the first encrypted letter of the pair is the one that lies on the same row as the first plaintext letter.
 
-        b. If the letters appear on the same row of the table, replace them with the letters to their immediate right respectively 
+        b. If the letters appear on the same row of the table, replace them with the letters to their immediate right respectively
 		   (wrapping around to the left side of the row if a letter in the original pair was on the right side of the row).
 
-        c. If the letters appear on the same column of the table, replace them with the letters immediately below respectively 
+        c. If the letters appear on the same column of the table, replace them with the letters immediately below respectively
 		   (wrapping around to the top side of the column if a letter in the original pair was on the bottom side of the column).
 
 */
@@ -77,9 +77,9 @@ string FormatMessage(string message)
     }
 
     // STEP 2 in algorithm
-    for(int i=1;i<message.length();i++)
+    for(int i=1;i<message.length();i+=2)
          if(message[i-1] == message[i])
-            message = message.insert(i, "x"), i++;
+            message = message.insert(i, "x");
     // STEP 3 in algorithm
     if(message.length()%2)
         message += "x";
@@ -178,14 +178,14 @@ int main()
 	cout<<"Enter the number of keys:";
 	cin>>num_of_keys;
 	cin.get();
-	
+
 	cout<<"Enter the keys:";
 	for(int i=0;i<num_of_keys;i++)
 	    getline(cin, keys[i]);
 
     cout<<"Enter a message to be encrypted: ";
     getline(cin, message);
-    
+
     for(int i=0;i<num_of_keys;i++)
     {
         GenerateMatrix(keys[i]);
