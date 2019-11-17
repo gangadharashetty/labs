6fbad800c47708901da4aa3ef236a2ef6f015f84
gangadhara-at-820046810644 <gs@nixbees.com>
2019-11-17 16:53:14 +0530

    added labset5

--- cns/08. RC4/rc4.cpp
+++ cns/08. RC4/rc4.cpp
@@ -2,7 +2,6 @@
 Program: RC4 Algorithm
 Author: Gangadhara Shetty P J
 */
-
 #include<iostream>
 using namespace std;
 int main()
@@ -13,14 +12,14 @@ int main()
     cin>>plain;
     cout<<"Enter key:";
     cin>>key;
-
+	
     cout<<"plaintext in bytes : " ;
 	for(int i =0 ;i < plain.length() ; i++) cout<< (int)plain[i]<<" " ;
 	cout<<endl;
 
     for(int i=0;i<255;i++)
         s[i] = i, t[i] = (int)key[i % key.length()];
-
+	
     for(int i=0, j=0; i<256; i++){
         j = (j+s[i]+t[i])%256;
         int t = s[i],   s[i] = s[j],   s[j] = t;
@@ -37,7 +36,6 @@ int main()
         t= (s[i]+s[j])%256;
         k[l] = s[t];
     }
-
     cout<<"keystream in bytes : " ;
     for(int i =0 ;i < plain.length() ; i++) cout<< k[i]<<" " ;
     cout<<endl;
